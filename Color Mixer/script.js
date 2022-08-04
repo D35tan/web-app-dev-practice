e6b9fb09746f291095427fe75637693f5f033882
Tolga Durdagi <durdagi.tolga@gmail.com>
2022-08-04 17:07:21 +0200

    initial commit

---
+++ Color Mixer/script.js
@@ -0,0 +1,35 @@
+const redSlide = document.querySelector("#red");
+const greenSlide = document.querySelector("#green");
+const blueSlide = document.querySelector("#blue");
+const rgbCode = document.querySelector("#rgb-output");
+let colorPreview = document.body.style.backgroundColor;
+
+function setBackgroundColor() {
+  const color =
+    "rgb(" +
+    redSlide.value +
+    ", " +
+    greenSlide.value +
+    ", " +
+    blueSlide.value +
+    ")";
+  document.body.style.backgroundColor = color;
+  console.clear();
+  console.log(color);
+}
+function rgbCodeOutput() {
+  rgbCode.textContent =
+    "rgb:" +
+    " " +
+    redSlide.value +
+    " " +
+    greenSlide.value +
+    " " +
+    blueSlide.value;
+}
+
+setBackgroundColor();
+rgbCodeOutput();
+
+document.body.addEventListener("input", rgbCodeOutput);
+document.body.addEventListener("input", setBackgroundColor);
